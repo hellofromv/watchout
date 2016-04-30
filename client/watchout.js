015c50afb9f92c96ee3059a959fecd95fc067bb1
vickeetran <ptran.vicki@gmail.com>
2016-04-29 19:24:59 -0700

    made a moving mario on mousemove

--- client/watchout.js
+++ client/watchout.js
@@ -1,22 +1,33 @@
 var width = 960;
 var height = 500;
 var bombHeight = 40;
+var marioPosition = [50, 50];
 
 var svg = d3.select('body').append('svg')
     .attr('width', width)
     .attr('height', height)
   .append('g');
 
+//add mario
+var mario = svg.append('image')
+  .attr('class', 'mario')
+  .attr('x', marioPosition[0])
+  .attr('y', marioPosition[1])
+  .attr('height', bombHeight)
+  .attr('width', bombHeight)
+  .attr('xlink:href', 'mario.png')
+  .style('cursor', 'pointer');
+
 var bombPositions = [];
 var numberOfBombs = 15;
 
-var getMousePosition = function() {
-  var coordinates = [0, 0];
-  coordinates = d3.mouse(this);
-  console.log(coordinates);
+// var getMousePosition = function() {
+//   var coordinates = [0, 0];
+//   coordinates = d3.mouse(this);
+//   console.log(coordinates);
 
-  update(coordinates, 'mario', 'mario.png');
-};
+//   updateMario(coordinates);
+// };
 
 var randomizePositions = function(numberOfBombs) {
   for (var i = 0; i < numberOfBombs; i++) {
@@ -26,28 +37,65 @@ var randomizePositions = function(numberOfBombs) {
   }
 };  
 
-var update = function(positions, className, imageSource) {
+// var updateMario = function(positions) {
+//   // DATA JOIN
+//   // Join new data with old elements, if any.
+//   var character = svg.selectAll('.mario')
+//       .data(positions);
+
+//   // UPDATE
+//   // Update old elements as needed.
+//   character.attr('class', 'mario movable')
+//       .attr('transform', function(d) { return 'translate(' + d[0] + ',' + d[1] + ')'; } )
+//       .on('mousedown', getMousePosition)
+//       .style('cursor', 'pointer')
+//       .call(drag);
+
+//   // ENTER
+//   // Create new elements as needed.
+//   character.enter().append('image')
+//       .attr('class', 'mario movable')
+//       .attr('transform', function(d) { return 'translate(' + d[0] + ',' + d[1] + ')'; } )
+//       .on('mousedown', getMousePosition)
+//       .style('cursor', 'pointer')
+//       .call(drag);
+
+
+//   // ENTER + UPDATE
+//   // Appending to the enter selection expands the update selection to include
+//   // entering elements; so, operations on the update selection after appending to
+//   // the enter selection will apply to both entering and updating nodes.
+//   character.attr('height', bombHeight)
+//       .attr('width', bombHeight)
+//       .attr('xlink:href', imageSource);
+
+//   // EXIT
+//   // Remove old elements as needed.
+//   //bombs.exit().remove();
+// };
+
+
+var updateBombs = function(positions) {
 
   // DATA JOIN
   // Join new data with old elements, if any.
-  var character = svg.selectAll('.' + className)
+  var character = svg.selectAll('.bombs')
       .data(positions);
 
   // UPDATE
   // Update old elements as needed.
-  character.attr('class', className)
-      .transition().duration(1500)
+  character.attr('class', 'bombs')
+      .transition().duration(1000)
       .attr('x', function(d) { return d[0]; } )
       .attr('y', function(d) { return d[1]; } );
 
   // ENTER
   // Create new elements as needed.
   character.enter().append('image')
-      .attr('class', className)
-      .transition().duration(1500)
+      .attr('class', 'bombs')
+      .transition().duration(1000)
       .attr('x', function(d) { return d[0]; } )
       .attr('y', function(d) { return d[1]; } );
-
 
   // ENTER + UPDATE
   // Appending to the enter selection expands the update selection to include
@@ -55,23 +103,30 @@ var update = function(positions, className, imageSource) {
   // the enter selection will apply to both entering and updating nodes.
   character.attr('height', bombHeight)
       .attr('width', bombHeight)
-      .attr('xlink:href', imageSource);
+      .attr('xlink:href', 'bob-omb.png');
 
   // EXIT
   // Remove old elements as needed.
-  //bombs.exit().remove();
+  character.exit().remove();
 };
 
 //add Mario to svg
-var marioPosition = svg.on('mouseclick', getMousePosition);
 //update(marioPosition, 'mario', 'mario.png');
 
 //randomize and update bomb positions
 randomizePositions(numberOfBombs);
-update(bombPositions, 'bomb', 'bob-omb.png');
+updateBombs(bombPositions);
+
+d3.select('svg').on('mousemove', function() {
+  var coordinates = d3.mouse(this);
+  d3.select('.mario')
+    .attr('x', coordinates[0])
+    .attr('y', coordinates[1]);
+});
+
 
 // Grab a random sample of letters from the alphabet, in alphabetical order.
 setInterval(function() {
   randomizePositions(numberOfBombs);
-  update(bombPositions, 'bomb', 'bob-omb.png');
+  updateBombs(bombPositions);
 }, 1000);
