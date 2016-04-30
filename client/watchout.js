59b7bda0a07b29f336cb3f58d9266cccccba92e2
vickeetran <ptran.vicki@gmail.com>
2016-04-29 20:21:03 -0700

    Add current score timer

--- client/watchout.js
+++ client/watchout.js
@@ -1,11 +1,14 @@
 var width = 960;
 var height = 500;
+var bombPositions = [];
+var numberOfBombs = 15;
 var bombHeight = 40;
 var marioPosition = [50, 50];
 
 var svg = d3.select('body').append('svg')
     .attr('width', width)
     .attr('height', height)
+    .style('cursor', 'none')
   .append('g');
 
 //add mario
@@ -16,18 +19,7 @@ var mario = svg.append('image')
   .attr('height', bombHeight)
   .attr('width', bombHeight)
   .attr('xlink:href', 'mario.png')
-  .style('cursor', 'pointer');
-
-var bombPositions = [];
-var numberOfBombs = 15;
-
-// var getMousePosition = function() {
-//   var coordinates = [0, 0];
-//   coordinates = d3.mouse(this);
-//   console.log(coordinates);
-
-//   updateMario(coordinates);
-// };
+  .style('cursor', 'none');
 
 var randomizePositions = function(numberOfBombs) {
   for (var i = 0; i < numberOfBombs; i++) {
@@ -36,44 +28,6 @@ var randomizePositions = function(numberOfBombs) {
     bombPositions[i] = [randomX, randomY];
   }
 };  
-
-// var updateMario = function(positions) {
-//   // DATA JOIN
-//   // Join new data with old elements, if any.
-//   var character = svg.selectAll('.mario')
-//       .data(positions);
-
-//   // UPDATE
-//   // Update old elements as needed.
-//   character.attr('class', 'mario movable')
-//       .attr('transform', function(d) { return 'translate(' + d[0] + ',' + d[1] + ')'; } )
-//       .on('mousedown', getMousePosition)
-//       .style('cursor', 'pointer')
-//       .call(drag);
-
-//   // ENTER
-//   // Create new elements as needed.
-//   character.enter().append('image')
-//       .attr('class', 'mario movable')
-//       .attr('transform', function(d) { return 'translate(' + d[0] + ',' + d[1] + ')'; } )
-//       .on('mousedown', getMousePosition)
-//       .style('cursor', 'pointer')
-//       .call(drag);
-
-
-//   // ENTER + UPDATE
-//   // Appending to the enter selection expands the update selection to include
-//   // entering elements; so, operations on the update selection after appending to
-//   // the enter selection will apply to both entering and updating nodes.
-//   character.attr('height', bombHeight)
-//       .attr('width', bombHeight)
-//       .attr('xlink:href', imageSource);
-
-//   // EXIT
-//   // Remove old elements as needed.
-//   //bombs.exit().remove();
-// };
-
 
 var updateBombs = function(positions) {
 
@@ -110,9 +64,6 @@ var updateBombs = function(positions) {
   character.exit().remove();
 };
 
-//add Mario to svg
-//update(marioPosition, 'mario', 'mario.png');
-
 //randomize and update bomb positions
 randomizePositions(numberOfBombs);
 updateBombs(bombPositions);
@@ -122,11 +73,30 @@ d3.select('svg').on('mousemove', function() {
   d3.select('.mario')
     .attr('x', coordinates[0])
     .attr('y', coordinates[1]);
+
+  //checkCollision(coordinates);  
 });
 
 
-// Grab a random sample of letters from the alphabet, in alphabetical order.
+var checkCollisions = function(marioPosition) {
+  console.log(d3.selectAll('.bombs').attr('x'));
+};
+
+checkCollisions();
+
+
+
+//reset bomb positions every second
 setInterval(function() {
   randomizePositions(numberOfBombs);
   updateBombs(bombPositions);
 }, 1000);
+
+//timer
+var time = 0;
+setInterval(function() {
+  time += 10;
+  d3.select('.current').selectAll('span')
+    .data([1])
+    .text(time / 1000);
+}, 10);
